85f30d46f332b5ad4e6fcae30616633dd9a2d8d6
Lan <lanhikarixx@gmail.com>
2020-09-26 14:42:16 -0500

    create scripts to automate copying and diffing

---
+++ diff.py
@@ -0,0 +1,34 @@
+# pulls all changes in config files from system to repository
+import typing
+import os
+import sys
+import argparse
+import util
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=f"compares config files between repository and system \
+according to {util.LOCATIONS_PATH}",
+        epilog=f'''
+examples:
+{sys.argv[0]} --only vimrc .vim/plugin.conf
+{sys.argv[0]} --all
+        ''',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    util.argparse_add_process_all_or_only_options(parser)
+    args = parser.parse_args()
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+
+    filtered = None
+    if args.all:
+        filtered = []
+    if not args.all and args.only:
+        filtered = args.only
+
+    loc_dic = util.get_locations_dictionary('locations.txt')
+    for conf in loc_dic.keys():
+        if filtered and conf not in filtered: continue
+        os.system(f'diff -Naurs --color=always {loc_dic[conf]} {conf}')
