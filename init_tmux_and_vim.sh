9477d772e8db016bfaedac619bdf78451b173a53
Mohammed Alzakariya <lanhikarixx@gmail.com>
2025-05-26 19:20:13 +0300

    add script for initializing tmux and vim

---
+++ init_tmux_and_vim.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "This script requires some manual steps. See comments."
+
+# install tmp for tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source ~/.tmux.conf
+# manual:
+# to install plugins: prefix - I. Update them with prefix - U.
+
+# Get vim.plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# manual:
+# in vim, run :PlugInstall to install plugins.
