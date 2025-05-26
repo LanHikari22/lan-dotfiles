515b2ad804e44f3cc8eb606b28b8607a6f7cae7e
Mohammed Alzakariya <lanhikarixx@gmail.com>
2025-05-26 19:11:46 +0300

    add custom omz theme and omz plugin init script

---
+++ init_omz.sh
@@ -0,0 +1,4 @@
+cd ~/.oh-my-zsh/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
